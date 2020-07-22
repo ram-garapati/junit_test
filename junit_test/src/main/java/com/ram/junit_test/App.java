ee173f5d306757a388c45dc0cb32a4457e8c36f6
ramga <ramga@DESKTOP-ARAKO1O>
2020-07-22 10:47:15 +0530

    initial

---
+++ junit_test/src/main/java/com/ram/junit_test/App.java
@@ -0,0 +1,13 @@
+package com.ram.junit_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
